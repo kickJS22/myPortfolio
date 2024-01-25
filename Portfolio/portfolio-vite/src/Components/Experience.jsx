28b3973cfe3d31fc2dacaf50ddb039783fffa4b8
kickJS <kickjs@kali.kickJS>
2024-01-25 01:51:18 -0300

    fixing percentages on responsive

--- Portfolio/portfolio-vite/src/Components/Experience.jsx
+++ Portfolio/portfolio-vite/src/Components/Experience.jsx
@@ -26,15 +26,19 @@ export function Experience() {
         <ul id="experience">
             <h1>{lan.com.title}</h1>
             <article id="experinceContainer">
+            <section ref={Jref}>
+                <h3>60%</h3>
+                <h2>Javascript</h2>
+            <section ref={Nref}>
+                <h3>50%</h3>
+                <h2>Node.js</h2>
+            </section>
+            </section>
                 <section ref={Rref}>
                 <h3>40%</h3>
                  <h2>React.js</h2>
             </section>
 
-            <section ref={Jref}>
-                <h3>60%</h3>
-                <h2>Javascript</h2>
-            </section>
 
             <section ref={Href}>
                 <h3>70%</h3>
@@ -46,10 +50,6 @@ export function Experience() {
                 <h2>CSS</h2>
             </section>
 
-            <section ref={Nref}>
-                <h3>50%</h3>
-                <h2>Node.js</h2>
-            </section>
 
             <section ref={Sref}>
                 <h3>40%</h3>
@@ -57,8 +57,8 @@ export function Experience() {
             </section>
 
             <section ref={Pref}>
-                <h3>60%</h3>
-                <h2>Photoshop</h2>
+                <h3>40%</h3>
+                <h2>Python</h2>
             </section>
 
             </article>
